ec11d7a1d0d0925458f4f5c6c9d7575a7c076bee
Amitesh Singh <amitesh.singh@corvallis.it>
2019-09-18 11:01:42 +0200

    [change] dashboard + title updated

--- src/dashboad/Dashboard/Dashboard.js
+++ src/dashboad/Dashboard/Dashboard.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import clsx from 'clsx';
-import PropTypes from 'prop-types';
-
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
@@ -15,193 +13,177 @@ import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
+import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-
 import { mainListItems, secondaryListItems } from '../listItems/listItems';
 import Chart from '../Chart/index';
 import Deposits from '../Deposits/index';
 import Orders from '../Orders/index';
-import CopyRight from '../../common/CopyRight/index';
+import Copyright from '../../common/CopyRight/index';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: 'flex',
+        display: 'flex'
     },
     toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
+        paddingRight: 24
     },
     toolbarIcon: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'flex-end',
         padding: '0 8px',
-        ...theme.mixins.toolbar,
+        ...theme.mixins.toolbar
     },
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
         transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-        }),
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        })
     },
     appBarShift: {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
         transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-        }),
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        })
     },
     menuButton: {
-        marginRight: 36,
+        marginRight: 36
     },
     menuButtonHidden: {
-        display: 'none',
+        display: 'none'
     },
     title: {
-        flexGrow: 1,
+        flexGrow: 1
     },
     drawerPaper: {
         position: 'relative',
         whiteSpace: 'nowrap',
         width: drawerWidth,
         transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-        }),
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        })
     },
     drawerPaperClose: {
         overflowX: 'hidden',
         transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
         }),
         width: theme.spacing(7),
         [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(9),
-        },
+            width: theme.spacing(9)
+        }
     },
     appBarSpacer: theme.mixins.toolbar,
     content: {
         flexGrow: 1,
         height: '100vh',
-        overflow: 'auto',
+        overflow: 'auto'
     },
     container: {
         paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
+        paddingBottom: theme.spacing(4)
     },
     paper: {
         padding: theme.spacing(2),
         display: 'flex',
         overflow: 'auto',
-        flexDirection: 'column',
+        flexDirection: 'column'
     },
     fixedHeight: {
-        height: 240,
-    },
+        height: 240
+    }
 }));
 
-class Dashboard extends Component {
+export default function Dashboard() {
+    
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-    render() {
-        let { classes } = this.props;
-
-        let [open, setOpen] = React.useState(true);
-        let handleDrawerOpen = () => {
-            setOpen(true);
-        };
-        let handleDrawerClose = () => {
-            setOpen(false);
-        };
-        
-        let fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
-        return (
-            <div className={classes.root}>
-                <CssBaseline />
-                <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-                    <Toolbar className={classes.toolbar}>
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            // onClick={handleDrawerOpen}
-                            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                            >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-                            Dashboard
-                        </Typography>
-                        <IconButton color="inherit">
-                            <Badge badgeContent={4} color="secondary">
-                            <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-
-                <Drawer
-                    variant="permanent"
-                    classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-                    }}
-                    open={open}
-                    >
-                    <div className={classes.toolbarIcon}>
-                    <IconButton 
-                        // onClick={handleDrawerClose}
-                    >
-                        <ChevronLeftIcon />
-                    </IconButton>
-                    </div>
-                    <Divider />
-                        <List>{mainListItems}</List>
-                    <Divider />
-                    <List>{secondaryListItems}</List>
-                </Drawer>
-
-                <main className={classes.content}>
-                    <div className={classes.appBarSpacer} />
-                        <Container maxWidth="lg" className={classes.container}>
-                        <Grid container spacing={3}>
-                            {/* Chart */}
-                            <Grid item xs={12} md={8} lg={9}>
-                            <Paper className={fixedHeightPaper}>
-                                <Chart />
-                            </Paper>
-                            </Grid>
-                            {/* Recent Deposits */}
-                            <Grid item xs={12} md={4} lg={3}>
-                            <Paper className={fixedHeightPaper}>
-                                <Deposits />
-                            </Paper>
-                            </Grid>
-                            {/* Recent Orders */}
-                            <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                <Orders />
-                            </Paper>
-                            </Grid>
-                        </Grid>
-                    </Container>
-                    <CopyRight />
-                </main>
+    return (
+        <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+            <Toolbar className={classes.toolbar}>
+            <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                Dashboard
+            </Typography>
+            <IconButton color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+                </Badge>
+            </IconButton>
+            </Toolbar>
+        </AppBar>
+        <Drawer
+            variant="permanent"
+            classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+            }}
+            open={open}
+        >
+            <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+                <ChevronLeftIcon />
+            </IconButton>
             </div>
-        );
-    }
+            <Divider />
+            <List>{mainListItems}</List>
+            <Divider />
+            <List>{secondaryListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+            <Grid container spacing={3}>
+                {/* Chart */}
+                <Grid item xs={12} md={8} lg={9}>
+                <Paper className={fixedHeightPaper}>
+                    <Chart />
+                </Paper>
+                </Grid>
+                {/* Recent Deposits */}
+                <Grid item xs={12} md={4} lg={3}>
+                <Paper className={fixedHeightPaper}>
+                    <Deposits />
+                </Paper>
+                </Grid>
+                {/* Recent Orders */}
+                <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                    <Orders />
+                </Paper>
+                </Grid>
+            </Grid>
+            </Container>
+            <Copyright />
+        </main>
+        </div>
+    );
 }
-
-
-Dashboard.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(useStyles)(Dashboard);
-
--- src/dashboad/Title/Title.js
+++ src/dashboad/Title/Title.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
 import Typography from '@material-ui/core/Typography';
 
-class Title extends Component {
-    render() {
-        return (
-            <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                {this.props.children}
-            </Typography>
-        );
-    }    
+export default function Title(props) {
+    return (
+        <Typography component="h2" variant="h6" color="primary" gutterBottom>
+            {props.children}
+        </Typography>
+    );
 }
 
 Title.propTypes = {
-    children: PropTypes.node,
+  children: PropTypes.node,
 };
-
-export default Title;
